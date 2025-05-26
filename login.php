831280e26afe449393ef60a737f3a33e4fa9c9f4
dadinelli <dadi.martinelli@gmail.com>
2025-05-26 10:43:39 +0200

    Aggiunto stile ad area riservata

--- login.php
+++ login.php
@@ -4,12 +4,13 @@
 
     /*if(!isset($_SESSION['is_logged'])){
         $DOM = file_get_contents('html/login.html');
-        echo $DOM;
     }*/
     //else{
+        $_SESSION["ID_Cliente"] = 1;
+
         $DOM = file_get_contents("html/area-riservata.html");
         $cliente = $_SESSION["ID_Cliente"];
-        $wishlist_query = $pdo->prepare("SELECT * FROM Whishlist, Libri, Clienti WHERE Libro = ID_libro AND Cliente = :cliente");
+        $wishlist_query = $pdo->prepare("SELECT * FROM Wishlist, Libri, Clienti WHERE Libro = ID_libro AND Cliente = :cliente");
         $wishlist_query->bindParam(':cliente', $cliente, PDO::PARAM_STR);
         $wishlist_query->execute();
         $result = $wishlist_query->fetchAll(PDO::FETCH_ASSOC);
@@ -17,18 +18,29 @@
         if(count($result) > 0){
             $wishlist = book_display($result);
         }
+        else{
+            $wishlist = "<p>Nessun libro salvato, se ne vuoi salvare alcuni vai al <a href=\"catalogo.php\">catalogo</a>.</p>";
+        }
 
-        $result = "";
         $personal_query = $pdo->prepare("SELECT * FROM Clienti WHERE ID_Cliente = :cliente");
         $personal_query->bindParam(':cliente', $cliente, PDO::PARAM_STR);
         $personal_query->execute();
-        $result = $personal_query->fetchAll(PDO::FETCH_ASSOC);
+        $personal_result = $personal_query->fetch(PDO::FETCH_ASSOC);
 
+
+        if(count($personal_result) > 0){
+            $personal_data = "<li> Username: " .  $personal_result["Username"] . "</li>";
+            $personal_data = $personal_data . "<li> Nome:". $personal_result["Nome"] ."</li>";
+            $personal_data = $personal_data . "<li> Cognome: ". $personal_result["Cognome"] ."</li>";
+            $personal_data = $personal_data . "<li> Email: ". $personal_result["Email"] ."</li>";
+            $personal_data = $personal_data . "<li> Telefono:". $personal_result["Telefono"] ."</li>";
+        }
+        else{
+            $personal_data = "Nessun cliente";
+        }
+        
         $DOM = str_replace('###LISTA-WISHLIST###', $wishlist, $DOM);
-        $personal_data = "<li>".$result["Username"]."</li>";
-        $personal_data = "<li>".$result["Nome"]."</li>";
-        $personal_data = "<li>".$result["Cognome"]."</li>";
-        $personal_data = "<li>".$result["Email"]."</li>";
-        $personal_data = "<li>".$result["Telefono"]."</li>";
+        $DOM = str_replace('###DATI-PERSONALI###', $personal_data, $DOM);
     //}
+    echo $DOM;
 ?>
