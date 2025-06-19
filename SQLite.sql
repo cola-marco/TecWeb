1ed60e5ef1b1fac3a7cd7bdd8ae0cf4f2667c293
Enrico Bianchi <enrico.bianchi.2002@gmail.com>
2025-06-19 18:47:50 +0200

    aggiunti percorsi immagini nel dump sql

--- SQLite.sql
+++ SQLite.sql
@@ -46,15 +46,15 @@ CREATE TABLE Wishlist (
     FOREIGN KEY (Libro) REFERENCES Libri(ID_Libro) ON DELETE CASCADE
 );
 
-INSERT INTO Libri (Titolo, Autore, Casa_Editrice, Genere, Pubblicazione, Trama) VALUES 
-('Il nome della rosa', 'Umberto Eco', 'Bompiani', 'Storico', 1980, 'Un monaco francescano indaga su misteriosi omicidi in un monastero medievale, tra segreti e verità scomode.'),
-('1984', 'George Orwell', 'Secker & Warburg', 'Distopico', 1949, 'In un futuro totalitario, un uomo cerca la verità in una società oppressa dal Grande Fratello.'),
-('Orgoglio e pregiudizio', 'Jane Austin', 'Thomas Egerton', 'Romantico', 1813, 'La storia di Elizabeth Bennet e del suo difficile rapporto con il signor Darcy, tra orgoglio e pregiudizi.'),
-('Il signore degli anelli', 'J.R.R. Tolkien', 'Allen & Unwin', 'Fantasy', 1954, 'Un gruppo di eroi parte per distruggere un anello magico e salvare la Terra di Mezzo dalle tenebre.'),
-('Cento anni di solitudine', 'Gabriel Garcìa Màrquez', 'Sudamericana', 'Realismo Magico', 1967, 'La saga della famiglia Buendía in un villaggio immaginario ricco di eventi magici e simbolici.'),
-('Il piccolo principe', 'Antoine de Saint-Exupéry', 'Reynal & Hitchcock', 'Fiaba', 1943, 'Un aviatore incontra un bambino proveniente da un altro pianeta che gli insegna il senso della vita.'),
-('Delitto e castigo', 'Fëdor Dostoevskij', 'The Russian Messenger', 'Psicologico', 1866, 'Un giovane studente commette un omicidio e affronta le conseguenze morali del suo gesto.'),
-('La coscienza di Zeno', 'Italo Svevo', 'Cappelli', 'Romanzo introspettivo', 1923, 'Il protagonista racconta la propria vita attraverso sedute psicoanalitiche, cercando un senso alla sua esistenza.'),
-('Harry Potter e la pietra filosofale', 'J.K. Rowling', 'Bloomsbury', 'Fantasy', 1997, 'Un ragazzo scopre di essere un mago e inizia la sua avventura nella scuola di magia di Hogwarts.'),
-('Sulla strada', 'Jack Kerouac', 'Viking Press', 'Narrativa', 1957, 'Il racconto di un viaggio on the road negli Stati Uniti, simbolo della beat generation.');
+INSERT INTO Libri (Titolo, Autore, Image_path, Casa_Editrice, Genere, Pubblicazione, Trama) VALUES 
+('Il nome della rosa', 'Umberto Eco', 'Immagini/cover_68484e26e00496.19349912.jpg', 'Bompiani', 'Storico', 1980, 'Un monaco francescano indaga su misteriosi omicidi in un monastero medievale, tra segreti e verità scomode.'),
+('1984', 'George Orwell', 'Immagini/cover_68484e68a8e416.87839484.jpg', 'Secker & Warburg', 'Distopico', 1949, 'In un futuro totalitario, un uomo cerca la verità in una società oppressa dal Grande Fratello.'),
+('Orgoglio e pregiudizio', 'Jane Austin', 'Immagini/cover_68484e8a0269f4.65233254.jpg', 'Thomas Egerton', 'Romantico', 1813, 'La storia di Elizabeth Bennet e del suo difficile rapporto con il signor Darcy, tra orgoglio e pregiudizi.'),
+('Il signore degli anelli', 'J.R.R. Tolkien', 'Immagini/cover_68484f9b4c4193.78006937.jpg', 'Allen & Unwin', 'Fantasy', 1954, 'Un gruppo di eroi parte per distruggere un anello magico e salvare la Terra di Mezzo dalle tenebre.'),
+('Cento anni di solitudine', 'Gabriel Garcìa Màrquez', 'Immagini/cover_68484fb606fe50.94536858.jpg', 'Sudamericana', 'Realismo Magico', 1967, 'La saga della famiglia Buendía in un villaggio immaginario ricco di eventi magici e simbolici.'),
+('Il piccolo principe', 'Antoine de Saint-Exupéry', 'Immagini/cover_68484fc3304cc8.11359000.jpg', 'Reynal & Hitchcock', 'Fiaba', 1943, 'Un aviatore incontra un bambino proveniente da un altro pianeta che gli insegna il senso della vita.'),
+('Delitto e castigo', 'Fëdor Dostoevskij', 'Immagini/cover_68484fce60e470.93876299.jpg', 'The Russian Messenger', 'Psicologico', 1866, 'Un giovane studente commette un omicidio e affronta le conseguenze morali del suo gesto.'),
+('La coscienza di Zeno', 'Italo Svevo', 'Immagini/cover_68484fdb11a3e1.36429587.jpg', 'Cappelli', 'Romanzo introspettivo', 1923, 'Il protagonista racconta la propria vita attraverso sedute psicoanalitiche, cercando un senso alla sua esistenza.'),
+('Harry Potter e la pietra filosofale', 'J.K. Rowling', 'Immagini/cover_68484fe49c6423.11389143.jpg', 'Bloomsbury', 'Fantasy', 1997, 'Un ragazzo scopre di essere un mago e inizia la sua avventura nella scuola di magia di Hogwarts.'),
+('Sulla strada', 'Jack Kerouac', 'Immagini/cover_68484fee970171.95685111.jpg', 'Viking Press', 'Narrativa', 1957, 'Il racconto di un viaggio on the road negli Stati Uniti, simbolo della beat generation.');
 
